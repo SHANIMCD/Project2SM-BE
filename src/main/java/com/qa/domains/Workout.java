1791bb91c50046a0191836a8af0512d5560a0322
SHANIMCD <shanilmcdonald@gmail.com>
2020-07-08 15:55:39 +0100

    delete exercise unit test complete

--- src/main/java/com/qa/domains/Workout.java
+++ src/main/java/com/qa/domains/Workout.java
@@ -63,4 +63,40 @@ public class Workout {
 		this.exercises = exercises;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((exercises == null) ? 0 : exercises.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Workout other = (Workout) obj;
+		if (exercises == null) {
+			if (other.exercises != null)
+				return false;
+		} else if (!exercises.equals(other.exercises))
+			return false;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	
+
 }
--- src/test/java/com/qa/services/ExerciseServiceUnitTest.java
+++ src/test/java/com/qa/services/ExerciseServiceUnitTest.java
@@ -2,6 +2,10 @@ package com.qa.services;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +22,7 @@ import com.qa.repos.ExerciseRepo;
 public class ExerciseServiceUnitTest {
 	
 
-	private final Exercise exercise = new Exercise("squat", "strength", "anImage");
+	private final Exercise exercise = new Exercise("squat", "strength", "anImage", null);
 	
 	private Exercise savedExercise;
 	
@@ -30,17 +34,25 @@ public class ExerciseServiceUnitTest {
 	
 	@InjectMocks
 	private ExerciseService service;
+
+	private Long e_id;
 	
 	@Before
 	public void init() {
-		this.savedExercise = new Exercise(exercise.getName(), exercise.getCategory(), exercise.getImageMain());
+		this.savedExercise = new Exercise(exercise.getName(), exercise.getCategory(), exercise.getImageMain(), exercise.getWorkout());
 		this.savedExercise.setE_id(1);
-		
 	}
 	
 	@Test
 	public void testCreate() {
 		Mockito.when(this.repo.save(exercise)).thenReturn(savedExercise);
 		assertEquals(this.service.mapToDTO(savedExercise), service.create(exercise));
+	}
+	@Test
+	public void testDeleteAnExercise() {
+		when(this.repo.existsById(e_id)).thenReturn(true, false);
+		this.service.delete(e_id);
+		
+		verify(this.repo, times(1)).deleteById(e_id);
 	}
 }
